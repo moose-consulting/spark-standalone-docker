8a0beeed7f398a9e3437b7e3fc1d546ceb34eea3
kflansburg <kevin.flansburg@gmail.com>
2020-11-15 14:39:42 -0600

    Fix paths

--- init.sh
+++ init.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -x
 export PATH="$PATH:$SPARK_HOME/bin"
-export SPARK_DIST_CLASSPATH="$(/root/hadoop/bin/hadoop classpath):$SPARK_HOME/jars"
 
 APP_CLASS=$1
 shift
 echo "Running class $APP_CLASS with arguments: $@"
 
 echo "Launching Spark Master"
-nocache /root/spark/sbin/start-master.sh
+nocache /opt/spark/sbin/start-master.sh
 
 echo "Launching Spark Worker"
-nocache /root/spark/sbin/start-slave.sh spark://$HOSTNAME:7077
+nocache /opt/spark/sbin/start-slave.sh spark://$HOSTNAME:7077
 
 echo "Running Spark Submit"
 nocache spark-submit --deploy-mode client --master spark://$HOSTNAME:7077 --class $APP_CLASS /root/app/run.jar "$@"
